25f5c4faf4f54c40b4887a8fae077a758ad5eba2
Rafael Costa <rafaelsevla@gmail.com>
2019-08-18 23:16:09 -0300

    isolate components

---
+++ src/components/filter/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+
+const Filter = () => {
+  return (
+    <div>
+      <h3>Mostrar</h3>
+      <span>Todos</span> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
+    </div>
+  )
+}
+
+export default Filter
